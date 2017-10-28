4de264bfd3a4276a8f54331461ee139398555857
Evan Kesten <ebk46@cornell.edu>
2017-10-28 06:57:19 +0000

    added small script to do all cropping at once

---
+++ crop_all.sh
@@ -0,0 +1,3 @@
+python mole_crop.py images neg_moles neg_moles/neg_coords.csv
+python mole_crop.py images pos_moles pos_moles/pos_coords.csv
+python mole_crop.py caltech_faces neg_caltech neg_caltech/neg_caltech_coords.csv
